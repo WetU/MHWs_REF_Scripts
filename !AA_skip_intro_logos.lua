e60c8d202d11623932bbf13a9a0e59c97271f86d
WetU <50101064+WetU@users.noreply.github.com>
2025-04-10 00:31:02 +0900

    Add files via upload

--- !AA_skip_intro_logos.lua
+++ !AA_skip_intro_logos.lua
@@ -9,13 +9,9 @@ local Flow_field = GUI010001_type_def:get_field("_Flow");
 local Skip_field = GUI010001_type_def:get_field("_Skip");
 local EnableSkip_field = GUI010001_type_def:get_field("_EnableSkip");
 
-local AutoSaveConfirm_type_def = sdk.find_type_definition("app.LogoController.cAutoSaveConfirm");
-
 local FLOW_type_def = sdk.find_type_definition("app.GUI010001.FLOW");
 local STARTUP = FLOW_type_def:get_field("STARTUP"):get_data(nil); -- static
 local COPYRIGHT = FLOW_type_def:get_field("COPYRIGHT"):get_data(nil); -- static
-
-local TRUE_ptr = sdk.to_ptr(true);
 
 sdk.hook(GUI010001_type_def:get_method("guiVisibleUpdate"), function(args)
     thread.get_hook_storage()["this"] = sdk.to_managed_object(args[2]);
@@ -25,22 +21,4 @@ end, function()
     if (Flow > STARTUP and Flow <= COPYRIGHT) and EnableSkip_field:get_data(GUI010001) == true and Skip_field:get_data(GUI010001) == false then
         GUI010001:set_field("_Skip", true);
     end
-end);
-
-local isWaitConfirm = nil;
-sdk.hook(AutoSaveConfirm_type_def:get_method("enter"), nil, function(retval)
-    isWaitConfirm = (sdk.to_int64(retval) & 1) == 1;
-    return retval;
-end);
-
-sdk.hook(AutoSaveConfirm_type_def:get_method("<enter>b__0_1(System.Object, ace.GUIBaseCore)"), nil, function()
-    isWaitConfirm = false;
-end);
-
-sdk.hook(GUI010001_type_def:get_method("isInputDecideTriggerCore"), nil, function(retval)
-    if isWaitConfirm == true then
-        isWaitConfirm = nil;
-        return TRUE_ptr;
-    end
-    return retval;
 end);
