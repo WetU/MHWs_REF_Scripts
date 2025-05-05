315d4736ddd9848c8a556214635e9b3949e6fa72
WetU <50101064+WetU@users.noreply.github.com>
2025-05-05 18:09:59 +0900

    Add files via upload

--- Auto Skip Kill Cam.lua
+++ Auto Skip Kill Cam.lua
@@ -17,9 +17,12 @@ local TYPE = {
     MAX = TYPE_type_def:get_field("MAX"):get_data(nil)
 };
 
-local config = json.load_file("AutoSkipKillCam.json") or {enableKillCam = true};
+local config = json.load_file("AutoSkipKillCam.json") or {enableKillCam = true, enableEndScene = true};
 if config.enableKillCam == nil then
     config.enableKillCam = true;
+end
+if config.enableEndScene == nil then
+    config.enableEndScene = true;
 end
 
 local function saveConfig()
@@ -31,24 +34,30 @@ sdk.hook(Constants.QuestDirector_type_def:get_method("canPlayHuntCompleteCamera"
 end);
 
 sdk.hook(HunterQuestActionController_type_def:get_method("checkQuestActionEnable(app.mcHunterQuestActionController.QUEST_ACTION_TYPE)"), function(args)
-    if config.enableKillCam == true then
+    if config.enableEndScene == true then
         local storage = thread.get_hook_storage();
         storage.this = sdk.to_managed_object(args[2]);
         storage.actionType = sdk.to_int64(args[3]) & 0xFFFFFFFF;
     end
 end, function(retval)
-    if config.enableKillCam == true and sdk.to_int64(retval) & 1 == 1 then
+    if config.enableEndScene == true and sdk.to_int64(retval) & 1 == 1 then
         local storage = thread.get_hook_storage();
         showStamp_method:call(storage.this, storage.actionType);
     end
     return retval;
 end);
 
-sdk.hook(GUI020201_type_def:get_method("guiVisibleUpdate"), Constants.getObject, function()
+sdk.hook(GUI020201_type_def:get_method("guiVisibleUpdate"), function(args)
+    if config.enableEndScene == true then
+        thread.get_hook_storage()["this"] = sdk.to_managed_object(args[2]);
+    end
+end, function()
     local GUI020201 = thread.get_hook_storage()["this"];
-    local CurType = CurType_field:get_data(GUI020201);
-    if CurType ~= nil and CurType ~= TYPE.MAX and CurType ~= TYPE.START then
-        Constants.requestClose_method:call(GUI020201, false);
+    if GUI020201 ~= nil then
+        local CurType = CurType_field:get_data(GUI020201);
+        if CurType ~= nil and CurType ~= TYPE.START and CurType ~= TYPE.MAX then
+            Constants.requestClose_method:call(GUI020201, false);
+        end
     end
 end);
 
@@ -57,8 +66,16 @@ re.on_config_save(saveConfig);
 re.on_draw_ui(function()
     if imgui.tree_node("Auto Skip Kill Cam") == true then
 		local changed = false;
-		changed, config.enableKillCam = imgui.checkbox("Enable Skip Kill-Cam", config.enableKillCam);
-        if changed == true then
+        local reqSave = false;
+		changed, config.enableKillCam = imgui.checkbox("Enable skip kill-cam", config.enableKillCam);
+        if changed == true and reqSave ~= true then
+            reqSave = true;
+        end
+        changed, config.enableEndScene = imgui.checkbox("Enable skip quest end scene", config.enableEndScene);
+        if changed == true and reqSave ~= true then
+            reqSave = true;
+        end
+        if reqSave == true then
             saveConfig();
         end
 		imgui.tree_pop();
