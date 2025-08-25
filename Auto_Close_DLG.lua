07092144a30952d068093cdb92bcd92a8f73dd5b
WetU <50101064+WetU@users.noreply.github.com>
2025-08-25 23:49:31 +0900

    Add files via upload

--- Auto_Close_DLG.lua
+++ Auto_Close_DLG.lua
@@ -8,15 +8,16 @@ local thread = Constants.thread;
 local getThisPtr = Constants.getThisPtr;
 
 local GUI000003_type_def = sdk.find_type_definition("app.GUI000003");
-local NotifyWindowApp_field = GUI000003_type_def:get_field("_NotifyWindowApp");
+local GUI000009_NotifyWindowApp_field = GUI000003_type_def:get_field("_NotifyWindowApp");
 
-local GUISystemModuleNotifyWindowApp_type_def = NotifyWindowApp_field:get_type();
+local GUISystemModuleNotifyWindowApp_type_def = GUI000009_NotifyWindowApp_field:get_type();
 local get__CurInfoApp_method = GUISystemModuleNotifyWindowApp_type_def:get_method("get__CurInfoApp");
 local closeGUI_method = GUISystemModuleNotifyWindowApp_type_def:get_method("closeGUI");
 local isExistCurrentInfo_method = GUISystemModuleNotifyWindowApp_type_def:get_method("isExistCurrentInfo");
 
 local GUINotifyWindowInfoApp_type_def = get__CurInfoApp_method:get_return_type();
 local get_NotifyWindowId_method = GUINotifyWindowInfoApp_type_def:get_method("get_NotifyWindowId");
+local set_DefaultIndex_method = GUINotifyWindowInfoApp_type_def:get_method("set_DefaultIndex(System.Int32)");
 local isExistWindowEndFunc_method = GUINotifyWindowInfoApp_type_def:get_method("isExistWindowEndFunc");
 local endWindow_method = GUINotifyWindowInfoApp_type_def:get_method("endWindow(System.Int32)");
 local executeWindowEndFunc_method = GUINotifyWindowInfoApp_type_def:get_method("executeWindowEndFunc");
@@ -40,15 +41,28 @@ local function Contains(value)
 end
 
 sdk.hook(GUI000003_type_def:get_method("guiOpenUpdate"), getThisPtr, function()
-    local NotifyWindowApp = NotifyWindowApp_field:get_data(thread.get_hook_storage()["this_ptr"]);
-    if isExistCurrentInfo_method:call(NotifyWindowApp) == true then
-        local CurInfoApp = get__CurInfoApp_method:call(NotifyWindowApp);
-        if Contains(get_NotifyWindowId_method:call(CurInfoApp)) == true then
-            endWindow_method:call(CurInfoApp, 0);
-            if isExistWindowEndFunc_method:call(CurInfoApp) == true then
-                executeWindowEndFunc_method:call(CurInfoApp);
-            end
-            closeGUI_method:call(NotifyWindowApp);
+    local NotifyWindowApp = GUI000009_NotifyWindowApp_field:get_data(thread.get_hook_storage()["this_ptr"]);
+    local CurInfoApp = get__CurInfoApp_method:call(NotifyWindowApp);
+    if CurInfoApp ~= nil and Contains(get_NotifyWindowId_method:call(CurInfoApp)) == true then
+        endWindow_method:call(CurInfoApp, 0);
+        if isExistWindowEndFunc_method:call(CurInfoApp) == true then
+            executeWindowEndFunc_method:call(CurInfoApp);
         end
+        closeGUI_method:call(NotifyWindowApp);
+    end
+end);
+
+local GUI000004_type_def = sdk.find_type_definition("app.GUI000004");
+local GUI000004_NotifyWindowApp_field = GUI000004_type_def:get_field("_NotifyWindowApp");
+local ListCtrl_field = GUI000004_type_def:get_field("_ListCtrl");
+
+local requestSelectIndexCore_method = sdk.find_type_definition("ace.cGUIInputCtrl_ScrollList`2<app.GUIID.ID,app.GUIFunc.TYPE>"):get_method("requestSelectIndexCore(System.Int32, System.Int32)");
+
+local EQUIP_003 = NotifyWindowID_type_def:get_field("EQUIP_003"):get_data(nil);
+
+sdk.hook(GUI000004_type_def:get_method("onOpen"), getThisPtr, function()
+    local this_ptr = thread.get_hook_storage()["this_ptr"];
+    if get_NotifyWindowId_method:call(get__CurInfoApp_method:call(GUI000004_NotifyWindowApp_field:get_data(this_ptr))) == EQUIP_003 then
+        requestSelectIndexCore_method:call(ListCtrl_field:get_data(this_ptr), 2, 2);
     end
 end);
