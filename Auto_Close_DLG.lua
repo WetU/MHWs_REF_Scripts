96ea0fe4428f398a2dc4df2b70390fc858c078c0
WetU <50101064+WetU@users.noreply.github.com>
2025-08-27 13:20:41 +0900

    Add files via upload

--- Auto_Close_DLG.lua
+++ Auto_Close_DLG.lua
@@ -16,16 +16,14 @@ local function saveConfig()
 end
 
 local GUI000003_type_def = sdk.find_type_definition("app.GUI000003");
-local GUI000009_NotifyWindowApp_field = GUI000003_type_def:get_field("_NotifyWindowApp");
+local GUI000003_NotifyWindowApp_field = GUI000003_type_def:get_field("_NotifyWindowApp");
 
-local GUISystemModuleNotifyWindowApp_type_def = GUI000009_NotifyWindowApp_field:get_type();
+local GUISystemModuleNotifyWindowApp_type_def = GUI000003_NotifyWindowApp_field:get_type();
 local get__CurInfoApp_method = GUISystemModuleNotifyWindowApp_type_def:get_method("get__CurInfoApp");
 local closeGUI_method = GUISystemModuleNotifyWindowApp_type_def:get_method("closeGUI");
-local isExistCurrentInfo_method = GUISystemModuleNotifyWindowApp_type_def:get_method("isExistCurrentInfo");
 
 local GUINotifyWindowInfoApp_type_def = get__CurInfoApp_method:get_return_type();
 local get_NotifyWindowId_method = GUINotifyWindowInfoApp_type_def:get_method("get_NotifyWindowId");
-local set_DefaultIndex_method = GUINotifyWindowInfoApp_type_def:get_method("set_DefaultIndex(System.Int32)");
 local isExistWindowEndFunc_method = GUINotifyWindowInfoApp_type_def:get_method("isExistWindowEndFunc");
 local endWindow_method = GUINotifyWindowInfoApp_type_def:get_method("endWindow(System.Int32)");
 local executeWindowEndFunc_method = GUINotifyWindowInfoApp_type_def:get_method("executeWindowEndFunc");
@@ -33,6 +31,7 @@ local executeWindowEndFunc_method = GUINotifyWindowInfoApp_type_def:get_method("
 local NotifyWindowID_type_def = get_NotifyWindowId_method:get_return_type();
 local auto_close_IDs = {
     NotifyWindowID_type_def:get_field("GUI000002_0000"):get_data(nil),
+    NotifyWindowID_type_def:get_field("GUI040502_0301"):get_data(nil),
     NotifyWindowID_type_def:get_field("GUI070000_DLG02"):get_data(nil),
     NotifyWindowID_type_def:get_field("GUI080301_0005_DLG"):get_data(nil),
     NotifyWindowID_type_def:get_field("GUI080301_0006_DLG"):get_data(nil),
@@ -49,7 +48,7 @@ local function Contains(value)
 end
 
 sdk.hook(GUI000003_type_def:get_method("guiOpenUpdate"), getThisPtr, function()
-    local NotifyWindowApp = GUI000009_NotifyWindowApp_field:get_data(thread.get_hook_storage()["this_ptr"]);
+    local NotifyWindowApp = GUI000003_NotifyWindowApp_field:get_data(thread.get_hook_storage()["this_ptr"]);
     local CurInfoApp = get__CurInfoApp_method:call(NotifyWindowApp);
     if CurInfoApp ~= nil then
         local Id = get_NotifyWindowId_method:call(CurInfoApp);
