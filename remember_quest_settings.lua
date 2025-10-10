07e15f4f51844378297a5bcd19d99feb08a48c35
WetU <50101064+WetU@users.noreply.github.com>
2025-10-10 16:24:04 +0900

    Add files via upload

--- remember_quest_settings.lua
+++ remember_quest_settings.lua
@@ -1,66 +1,67 @@
 local Constants = _G.require("Constants/Constants");
 
-local type = Constants.type;
-local table = Constants.table;
+local pairs = Constants.pairs;
 
-local json = Constants.json;
 local sdk = Constants.sdk;
-local re = Constants.re;
 local thread = Constants.thread;
 
 local getThisPtr = Constants.getThisPtr;
-
-local config = json.load_file("remember_quest_settings.json") or {view_type = 0, sort_types = {-1, -1, 0, 2, 0, 0, 2, 2, -1, 2, 2, 2, 2}};
-if config.view_type == nil or type(config.view_type) ~= "number" then
-    config.view_type = 0;
-end
-if config.sort_types == nil or type(config.sort_types) ~= "table" then
-    config.sort_types = {-1, -1, 0, 2, 0, 0, 2, 2, -1, 2, 2, 2, 2};
-end
-
-local function saveConfig()
-    json.dump_file("remember_quest_settings.json", config);
-end
 
 local GUI050000_type_def = sdk.find_type_definition("app.GUI050000");
 local get_QuestCounterContext_method = GUI050000_type_def:get_method("get_QuestCounterContext");
 
 local QuestCounterContext_type_def = get_QuestCounterContext_method:get_return_type();
 local QuestViewType_field = QuestCounterContext_type_def:get_field("QuestViewType");
-local QuestCategorySortType_field = QuestCounterContext_type_def:get_field("QuestCategorySortType");
 
-local SORT_TYPE_type_def = sdk.find_type_definition("app.GUI050000QuestListParts.SORT_TYPE");
-local value_field = SORT_TYPE_type_def:get_field("value__");
+local GRID = QuestViewType_field:get_type():get_field("GRID"):get_data(nil);
+
+local GUI050000QuestListParts_type_def = sdk.find_type_definition("app.GUI050000QuestListParts");
+local get_ViewCategory_method = GUI050000QuestListParts_type_def:get_method("get_ViewCategory");
+local setSortDifficulty_method = GUI050000QuestListParts_type_def:get_method("setSortDifficulty(System.Boolean, System.Boolean)");
+local setSortNewest_method = GUI050000QuestListParts_type_def:get_method("setSortNewest(System.Boolean)");
+local PNLChangeSortType_field = GUI050000QuestListParts_type_def:get_field("_PNLChangeSortType");
+
+local set_Message_method = PNLChangeSortType_field:get_type():get_method("set_Message(System.String)");
+
+local CATEGORY_type_def = get_ViewCategory_method:get_return_type();
+local SortNewest = {
+    CATEGORY_type_def:get_field("DECLARATION_HISTORY"):get_data(nil),
+    CATEGORY_type_def:get_field("KEEP_QUEST"):get_data(nil)
+};
+local SortDifficulty = {
+    CATEGORY_type_def:get_field("STORY"):get_data(nil),
+    CATEGORY_type_def:get_field("FREE"):get_data(nil),
+    CATEGORY_type_def:get_field("EVENT"):get_data(nil),
+    CATEGORY_type_def:get_field("ARENA"):get_data(nil),
+    CATEGORY_type_def:get_field("FREE_TA"):get_data(nil),
+    CATEGORY_type_def:get_field("CHALLENGE"):get_data(nil),
+    CATEGORY_type_def:get_field("RECRUITMENT_LOBBY"):get_data(nil),
+    CATEGORY_type_def:get_field("LINK_MEMBER"):get_data(nil),
+    CATEGORY_type_def:get_field("SERCH_RESCUE_SIGNAL"):get_data(nil)
+};
+
+sdk.hook(GUI050000QuestListParts_type_def:get_method("sortQuestDataList(System.Boolean)"), getThisPtr, function()
+    local this_ptr = thread.get_hook_storage()["this_ptr"];
+    local curCategory = get_ViewCategory_method:call(this_ptr);
+    for _, v in pairs(SortNewest) do
+        if v == curCategory then
+            setSortNewest_method:call(this_ptr, false);
+            set_Message_method:call(PNLChangeSortType_field:get_data(this_ptr), "새로운 순");
+            return;
+        end
+    end
+    for _, v in pairs(SortDifficulty) do
+        if v == curCategory then
+            setSortDifficulty_method:call(this_ptr, false, false);
+            set_Message_method:call(PNLChangeSortType_field:get_data(this_ptr), "난이도 높은 순");
+            return;
+        end
+    end
+end);
 
 sdk.hook(GUI050000_type_def:get_method("onOpen"), getThisPtr, function()
     local QuestCounterContext = get_QuestCounterContext_method:call(thread.get_hook_storage()["this_ptr"]);
-    local sort_type_list = QuestCategorySortType_field:get_data(QuestCounterContext);
-    for i = 0, sort_type_list:get_size() - 1 do
-        local sort_type = sort_type_list:get_element(i);
-        local saved_sort_type = config.sort_types[i + 1];
-        if value_field:get_data(sort_type) ~= saved_sort_type then
-            sdk.set_native_field(sort_type, SORT_TYPE_type_def, "value__", saved_sort_type);
-            sort_type_list[i] = sort_type;
-        end
-    end
-    local view_type = config.view_type;
-    if QuestViewType_field:get_data(QuestCounterContext) ~= view_type then
-        sdk.set_native_field(QuestCounterContext, QuestCounterContext_type_def, "QuestViewType", view_type);
+    if QuestViewType_field:get_data(QuestCounterContext) ~= GRID then
+        sdk.set_native_field(QuestCounterContext, QuestCounterContext_type_def, "QuestViewType", GRID);
     end
 end);
-
-sdk.hook(GUI050000_type_def:get_method("closeQuestDetailWindow"), getThisPtr, function()
-    local QuestCounterContext = get_QuestCounterContext_method:call(thread.get_hook_storage()["this_ptr"]);
-    local sort_type_list = QuestCategorySortType_field:get_data(QuestCounterContext);
-    config.sort_types = {};
-    for i = 0, sort_type_list:get_size() - 1 do
-        table.insert(config.sort_types, value_field:get_data(sort_type_list:get_element(i)));
-    end
-    local QuestViewType = QuestViewType_field:get_data(QuestCounterContext);
-    if QuestViewType ~= config.view_type then
-        config.view_type = QuestViewType;
-    end
-    saveConfig();
-end);
-
-re.on_config_save(saveConfig);
