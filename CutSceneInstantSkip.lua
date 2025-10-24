55d0a064960f301e0793d1ffb7068cfad4193816
WetU <50101064+WetU@users.noreply.github.com>
2025-10-25 00:02:54 +0900

    Add files via upload

--- CutSceneInstantSkip.lua
+++ CutSceneInstantSkip.lua
@@ -1,14 +1,35 @@
 local Constants = _G.require("Constants/Constants");
 
+local ipairs = Constants.ipairs;
+
 local sdk = Constants.sdk;
 local thread = Constants.thread;
 
-local getThisPtr = Constants.getThisPtr;
-
 local GUI020025_type_def = sdk.find_type_definition("app.GUI020025");
+local get_CurrentDispPattern_method = GUI020025_type_def:get_method("get_CurrentDispPattern");
 local doSkip_method = GUI020025_type_def:get_method("doSkip");
 
-sdk.hook(GUI020025_type_def:get_method("updateSkipOnTimer"), getThisPtr, function(retval)
-    doSkip_method:call(thread.get_hook_storage()["this_ptr"]);
+local DISP_PATTERN_type_def = get_CurrentDispPattern_method:get_return_type();
+local DISP_PATTERN = {
+    DISP_PATTERN_type_def:get_field("CUT_SCENE"):get_data(nil),
+    DISP_PATTERN_type_def:get_field("CUT_SCENE_MULTI"):get_data(nil)
+};
+
+local shouldSkip = false;
+sdk.hook(GUI020025_type_def:get_method("updateSkipOnTimer"), function(args)
+    local this_ptr = args[2];
+    local curDispPattern = get_CurrentDispPattern_method:call(this_ptr);
+    for _, v in ipairs(DISP_PATTERN) do
+        if curDispPattern == v then
+            thread.get_hook_storage()["this_ptr"] = this_ptr;
+            shouldSkip = true;
+            break;
+        end
+    end
+end, function(retval)
+    if shouldSkip == true then
+        doSkip_method:call(thread.get_hook_storage()["this_ptr"]);
+        shouldSkip = false;
+    end
     return retval;
 end);
