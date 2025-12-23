b53fa917712765e6967c8042760614404c1716f2
WetU <50101064+WetU@users.noreply.github.com>
2025-12-23 22:17:26 +0900

    Add files via upload

--- AutoSkipKillCam.lua
+++ AutoSkipKillCam.lua
@@ -18,15 +18,18 @@ local QUEST_ACTION_TYPE = {
 };
 
 local GUI020201_type_def = sdk.find_type_definition("app.GUI020201");
+local StampPanels_field = GUI020201_type_def:get_field("_StampPanels");
 local GUI020201_CurType_field = GUI020201_type_def:get_field("_CurType");
-local GUI_field = GUI020201_type_def:get_field("_GUI");
-
-local set_PlaySpeed_method = GUI_field:get_type():get_method("set_PlaySpeed(System.Single)");
-
-local GUI020216_type_def = sdk.find_type_definition("app.GUI020216");
-local GUI020216_CurType_field = GUI020216_type_def:get_field("_CurType");
 
 local TYPE_MAX = GUI020201_CurType_field:get_type():get_field("MAX"):get_data(nil);
+
+local GUI020216_type_def = sdk.find_type_definition("app.GUI020216");
+local Panel_field = GUI020216_type_def:get_field("_Panel");
+local GUI020216_CurType_field = GUI020216_type_def:get_field("_CurType");
+
+local get_Component_method = Panel_field:get_type():get_method("get_Component");
+
+local set_PlaySpeed_method = get_Component_method:get_return_type():get_method("set_PlaySpeed(System.Single)");
 
 local FALSE_ptr = sdk.to_ptr(false);
 local ZERO_float_ptr = sdk.float_to_ptr(0.0);
@@ -59,73 +62,54 @@ sdk.hook(HunterQuestActionController_type_def:get_method("requestDelayStamp(app.
     end
 end);
 
-local GUI020201_datas = {
+local hook_datas = {
     GUI = nil,
-    reqSkip = false,
-    isSetted = false
+    reqSkip = nil,
+    isSetted = nil
 };
+
+local function postHook_guiVisibleUpdate()
+    if hook_datas.reqSkip == true and hook_datas.isSetted ~= true then
+        set_PlaySpeed_method:call(hook_datas.GUI, 10.0);
+        hook_datas.isSetted = true;
+        hook_datas.reqSkip = nil;
+    end
+end
+
+local function postHook_onCloseApp()
+    if hook_datas.isSetted == true then
+        set_PlaySpeed_method:call(hook_datas.GUI, 1.0);
+        hook_datas.isSetted = nil;
+        hook_datas.GUI = nil;
+    end
+end
 
 sdk.hook(GUI020201_type_def:get_method("onOpen"), getThisPtr, function()
     local GUI020201_ptr = thread.get_hook_storage()["this_ptr"];
     if GUI020201_CurType_field:get_data(GUI020201_ptr) ~= TYPE_MAX then
-        if GUI020201_datas.GUI == nil then
-            GUI020201_datas.GUI = GUI_field:get_data(GUI020201_ptr);
-        end
-        GUI020201_datas.reqSkip = true;
+        hook_datas.GUI = get_Component_method:call(StampPanels_field:get_data(GUI020201_ptr):get_element(0));
+        hook_datas.reqSkip = true;
     end
 end);
 
-sdk.hook(GUI020201_type_def:get_method("guiVisibleUpdate"), nil, function()
-    if GUI020201_datas.reqSkip == true and GUI020201_datas.isSetted == false then
-        GUI020201_datas.isSetted = true;
-        GUI020201_datas.reqSkip = false;
-        set_PlaySpeed_method:call(GUI020201_datas.GUI, 10.0);
-    end
-end);
+sdk.hook(GUI020201_type_def:get_method("guiVisibleUpdate"), nil, postHook_guiVisibleUpdate);
 
-sdk.hook(GUI020201_type_def:get_method("onCloseApp"), nil, function()
-    if GUI020201_datas.isSetted == true then
-        GUI020201_datas.isSetted = false;
-        set_PlaySpeed_method:call(GUI020201_datas.GUI, 1.0);
-    end
-end);
-
-local GUI020216_datas = {
-    GUI = nil,
-    reqSkip = false,
-    isSetted = false
-};
+sdk.hook(GUI020201_type_def:get_method("onCloseApp"), nil, postHook_onCloseApp);
 
 sdk.hook(GUI020216_type_def:get_method("onOpen"), getThisPtr, function()
     local GUI020216_ptr = thread.get_hook_storage()["this_ptr"];
     if GUI020216_CurType_field:get_data(GUI020216_ptr) ~= TYPE_MAX then
-        if GUI020216_datas.GUI == nil then
-            GUI020216_datas.GUI = GUI_field:get_data(GUI020216_ptr);
-        end
-        GUI020216_datas.reqSkip = true;
+        hook_datas.GUI = get_Component_method:call(Panel_field:get_data(GUI020216_ptr));
+        hook_datas.reqSkip = true;
     end
 end);
 
-sdk.hook(GUI020216_type_def:get_method("guiVisibleUpdate"), nil, function()
-    if GUI020216_datas.reqSkip == true and GUI020216_datas.isSetted == false then
-        GUI020216_datas.isSetted = true;
-        GUI020216_datas.reqSkip = false;
-        set_PlaySpeed_method:call(GUI020216_datas.GUI, 10.0);
-    end
-end);
+sdk.hook(GUI020216_type_def:get_method("guiVisibleUpdate"), nil, postHook_guiVisibleUpdate);
 
-sdk.hook(GUI020216_type_def:get_method("onCloseApp"), nil, function()
-    if GUI020216_datas.isSetted == true then
-        GUI020216_datas.isSetted = false;
-        set_PlaySpeed_method:call(GUI020216_datas.GUI, 1.0);
-    end
-end);
+sdk.hook(GUI020216_type_def:get_method("onCloseApp"), nil, postHook_onCloseApp);
 
 re.on_script_reset(function()
-    if GUI020201_datas.isSetted == true then
-        set_PlaySpeed_method:call(GUI020201_datas.GUI, 1.0);
-    end
-    if GUI020216_datas.isSetted == true then
-        set_PlaySpeed_method:call(GUI020216_datas.GUI, 1.0);
+    if hook_datas.isSetted == true then
+        set_PlaySpeed_method:call(hook_datas.GUI, 1.0);
     end
 end);
